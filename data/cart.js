b249b17c6ae29eaa78b0695a9a6d36ee69a6489a
sainath161 <sainathsmart1234@gmail.com>
2024-01-08 23:25:07 +0530

    third attempt

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,89 @@
-const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      // deliveryOptionId: '1'
+    });
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
+
+// Note: this code is not in the video. This code is for
+// people who are starting straight from lesson 16. This
+// checks if the cart in localStorage has deliveryOptionIds.
+// If not, it will add a deliveryOptionId and refresh the page.
+function validateCart() {
+  if (!cart) return;
+
+  let needsPageRefresh;
+  for (let i = 0; i < cart.length; i++) {
+    if (!cart[i].deliveryOptionId) {
+      cart[i].deliveryOptionId = '1';
+      needsPageRefresh = true;
+    }
+  }
+
+  if (needsPageRefresh) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+    window.location.reload();
+  }
+}
+validateCart();
